34707ba6c47be975b66b1cb781e2c10e786ca6de
acuD1 <arsciand@student.42.fr>
2018-10-19 22:33:52 +0200

    Add header

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/14 11:08:12 by arsciand          #+#    #+#             */
+/*   Updated: 2018/10/16 14:51:06 by arsciand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+void	ft_putchar(char c);
+size_t	ft_strlen(char const *s);
+void    ft_putstr(char const *s);
+
+#endif
