46f826c3797bbe316620d2396b130731bd1535de
acuD1 <arsciand@student.42.fr>
2018-11-12 16:56:28 +0100

    Part I and II done

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,14 +6,11 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/14 19:14:12 by arsciand          #+#    #+#             */
-/*   Updated: 2018/11/12 15:06:16 by arsciand         ###   ########.fr       */
+/*   Updated: 2018/11/12 16:56:18 by arsciand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
