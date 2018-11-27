6676c888fa420f353d9ef4d5aac002aa1035ee6a
acuD1 <arsciand@student.42.fr>
2018-11-27 11:44:31 +0100

    Update ft_strjoin for performance

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/14 19:13:42 by arsciand          #+#    #+#             */
-/*   Updated: 2018/11/20 16:59:03 by arsciand         ###   ########.fr       */
+/*   Updated: 2018/11/27 11:42:42 by arsciand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	len;
 	char	*dst;
+	char	*tmp;
 
 	if (!s1 || !s2)
 		return (NULL);
 	len = ft_strlen(s1) + ft_strlen(s2);
 	if (!(dst = ft_memalloc(len + 1)))
 		return (NULL);
-	ft_strcpy(dst, s1);
-	ft_strcat(dst, s2);
-	return (dst);
+	tmp = dst;
+	while (*s1)
+		*dst++ = *s1++;
+	ft_strcpy(dst, s2);
+	return (dst - (dst - tmp));
 }
