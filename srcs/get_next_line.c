0e87cbbd52b9577ef42f572e403bf06be9e8b6e7
acuD1 <arsciand@student.42.fr>
2019-06-15 10:17:15 +0200

    Revert commit GNL

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 18:50:58 by arsciand          #+#    #+#             */
-/*   Updated: 2019/06/02 13:59:10 by arsciand         ###   ########.fr       */
+/*   Updated: 2019/06/15 10:17:03 by arsciand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,25 @@ static int	check_line(char **stack)
 	return (1);
 }
 
-static int	init(int const fd, char **line, char **stack)
+static int	init(const int fd, char **line, char *buff, char **stack)
 {
-	if (fd == -1 || FD_MAXSET > 4864 || BUFF_SIZE < 1 || !line
-			|| (!(stack[fd]) && !(stack[fd] = ft_strnew(0))))
+	if (fd == -1 || FD_MAXSET > 4864 || BUFF_SIZE < 1
+			|| !line || read(fd, buff, 0))
 		return (0);
+	if (!(stack[fd]))
+		if (!(stack[fd] = ft_strnew(0)))
+			return (0);
 	return (1);
 }
 
-int			get_next_line(int const fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
 	static char	*stack[FD_MAXSET];
-	char		buff[BUFF_SIZE + 1];
+	char		*buff;
 	char		*tmp;
 	int			ret;
 
-	if ((read(fd, buff, 0) == -1) || !(init(fd, line, stack)))
+	if (!(buff = ft_memalloc(BUFF_SIZE + 1)) || (!init(fd, line, buff, stack)))
 		return (-1);
 	while (!(check_line(&stack[fd])) && (ret = read(fd, buff, BUFF_SIZE)))
 	{
@@ -50,14 +53,16 @@ int			get_next_line(int const fd, char **line)
 		tmp = stack[fd];
 		stack[fd] = ft_strjoinf(tmp, buff, 1);
 	}
-	ft_bzero(buff, BUFF_SIZE);
+	free(buff);
 	*line = ft_strsub(stack[fd], 0, ft_strclen(stack[fd], '\n'));
 	if (*stack[fd])
 	{
 		if (check_line(&stack[fd]))
 			ft_strcpy(stack[fd], ft_strchr(stack[fd], '\n') + 1);
-		ft_strdel(&stack[fd]);
+		else
+			ft_strdel(&stack[fd]);
 		return (1);
 	}
+	free(stack[fd]);
 	return (0);
 }
